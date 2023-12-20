e7df5de251073dfe0e5f8ed2b54a5a1afcda8e49
gaechka <liza.schumova@gmail.com>
2023-12-20 05:11:24 +0300

    docker setup done

--- db/db.connection.js
+++ db/db.connection.js
@@ -1,6 +1,12 @@
 import DataBase from './db.class.js';
+import 'dotenv/config';
+const {
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+  } = process.env;
 
-const dbUrl = 'mongodb://localhost:27017/testdb';
+const dbUrl = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 const db = new DataBase(dbUrl);
 
 export default db;
